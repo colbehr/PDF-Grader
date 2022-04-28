e73ff7a434628819bf537e6cdbfdd28a2b7ffa07
colbehr <colbehr@gmail.com>
2022-04-28 16:21:59 -0700

    changed back start page to home

--- src/main/java/com/ezgrader/pdfgrader/PDFGrader.java
+++ src/main/java/com/ezgrader/pdfgrader/PDFGrader.java
@@ -21,7 +21,7 @@ public class PDFGrader extends Application {
     @Override
     public void start(Stage primaryStage) throws Exception{
         ResourceBundle resources = ResourceBundle.getBundle("fa.fontawesome");
-        GridPane root = FXMLLoader.load(getClass().getResource("/com/ezgrader/pdfgrader/export.fxml"), resources);
+        GridPane root = FXMLLoader.load(getClass().getResource("/com/ezgrader/pdfgrader/home.fxml"), resources);
         MakeStretchy(root);
 
         primaryStage.setTitle("PDF Grader");
